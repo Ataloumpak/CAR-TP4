1f870b68909b8980316933da7daf42675d2c0350
Xeheros <xeheros@gmail.com>
2014-05-08 17:38:03 +0200

    Servlet bugé

---
+++ src/java/servlet/SaveBook.java
@@ -0,0 +1,56 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.ejb.EJB;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import livres.Bibliothecaire;
+
+/**
+ * Servlet implementation class SaveBook
+ */
+@WebServlet("/SaveBook")
+public class SaveBook extends HttpServlet {
+
+    @EJB
+    private Bibliothecaire bibliothecaire;
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SaveBook() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     * response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        bibliothecaire.ajouterLivres();
+        ArrayList<String> auteurs = (ArrayList<String>) bibliothecaire.getAuteurs();
+
+        for (String auteur : auteurs) {
+            out.println(auteur);
+        }
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     * response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+    }
+
+}
